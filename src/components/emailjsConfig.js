8dcae86cc0503ae681f11f006710bc593cf584b2
Nicholas Chihanga <142257785+MilkyLane@users.noreply.github.com>
2025-01-05 22:44:29 +0200

    Updated emailjsConfig.js

--- src/components/emailjsConfig.js
+++ src/components/emailjsConfig.js
@@ -1,12 +1,12 @@
 // emailjsConfig.js
 export const EMAILJS_CONFIG = {
     // Your EmailJS service ID
-    serviceID: 'service_043e4lt',
+    serviceID: '...',
     
     // Your EmailJS template ID
-    templateID: 'template_ovc3hfm',
+    templateID: '...',
     
     // Your EmailJS user ID
-    userID: 'nickD4eLXZC3djphE6Izh',
+    userID: '...',
   };
   
